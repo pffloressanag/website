8b76d39e25057c9d99ac509f7ab79533510dd276
Ryan S <60458664+RyanSpadt@users.noreply.github.com>
2021-02-16 21:05:06 -0500

    Update settings.py
    Allow local host.

--- website_project/website_project/settings.py
+++ website_project/website_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY']   # Instead of your actual secret key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
